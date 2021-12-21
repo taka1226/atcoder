1065e0c4e45ab2daaf1090675238ba756466f6f4
Takahiro Yamaji <taka2637napoleon815@icloud.com>
2021-12-21 19:51:02 +0900

    仕切り問題はbit 全探索

---
+++ ABC197/C/main.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+//const
+const int MAX_N = 25;
+//input
+int N;
+ll A[MAX_N];
+
+//par[i] = i の後にしきりがある
+bool par[MAX_N];
+int main(){
+    cin >> N;
+    for (int i=0;i<N;i++){
+        cin >> A[i];
+    }
+
+    ll ans;
+    ll min_ans = 1e15;
+    for (int bit = 0;bit < (1<<N);bit++){
+        vector<bool> V(N);
+        ans = 0;
+        ll tmp = 0;
+        for (int i=0;i<N;i++){
+            tmp |= A[i];
+            if (bit & (1 << i)){
+                ans ^= tmp;
+                tmp = 0;
+            }
+        }
+
+        ans ^= tmp;
+        if (min_ans > ans){
+            min_ans = ans;
+        }
+    }
+
+    cout << min_ans << endl;
+
+    return 0;
+}
