e791297579246359ea51408416c4e35d5a30f91f
taka1226 <taka2637napoleon815@icloud.com>
2022-05-15 00:00:20 +0900

    ABC251 E まで完了

--- ABC251/D/main.cpp
+++ ABC251/D/main.cpp
@@ -7,17 +7,25 @@ int W;
 int main(){
     cin >> W;
 
-    int hw = (W + 1) / 2;
-
-    int n = 1;
     vector<int> ans;
     ans.push_back(1);
-    while (n < hw){
-        n *= 2;
-        ans.push_back(n);
+    for (int i=1;i<100;i++){
+        ans.push_back(i);
     }
 
-    
+    for (int i=1;i<100;i++){
+        ans.push_back(i * 1e2);
+    }
+
+    for (int i=1;i<100;i++){
+        ans.push_back(i * 1e4);
+    }
+
+    cout << ans.size() << endl;
+    for (auto ele : ans){
+        cout << ele << " ";
+    }
+    cout << endl;
 
     return 0;
 }
--- ABC251/E/main.cpp
+++ ABC251/E/main.cpp
@@ -9,80 +9,43 @@ const int MAX_N = 300005;
 int N;
 ll A[MAX_N];
 
+ll dp[MAX_N][2]; //dp[i][j] は i まで分かっていて iはj回行う
+
 int main(){
     cin >> N;
-    for (int i=0;i<N;i++){
+    for (int i=1;i<=N;i++){
         cin >> A[i];
     }
 
-
-    if (N % 2 == 0){
-        ll ans1 = 0;
-        for (int i=0;i<N;i+=2){
-            ans1 += A[i];
-        }
-        ll ans2 = 0;
-        for (int i=1;i<N;i+=2){
-            ans2 += A[i];
-        }
-
-        cout << min(ans1, ans2) << endl;
-    }else if (N % 2 == 1){
-        ll ans1 = 0;
-        ll ans2 = 0;
-        ll ans3 = 0;
-        ll ans4 = 0;
-        ll ans5 = 0;
-
-        vector<ll> middle;
-        for (int i=2;i<N-1;i+=2){
-            middle.push_back(i);
-        }
-
-        vector<ll> tmp1 = middle;
-        tmp1.push_back(0);
-        tmp1.push_back(N-2);
-        ll s1 = 0;
-        for (auto u : tmp1){
-            s1 += A[u];
-        }
-
-        vector<ll> tmp2 = middle;
-        tmp2.push_back(N-1);
-        tmp2.push_back(1);
-        ll s2 = 0;
-        for (auto u : tmp2){
-            s2 += A[u];
-        }
-
-        vector<ll> tmp3 = middle;
-        tmp3.push_back(0);
-        tmp3.push_back(N-1);
-        ll s3 = 0;
-        for (auto u : tmp3){
-            s3 += A[u];
-        }
-
-        vector<ll> middle2;
-        for (int i=1;i<N;i+=2){
-            middle2.push_back(i);
-        }
-
-        vector<ll> tmp4 = middle2;
-        tmp4.push_back(0);
-        ll s4 = 0;
-        for (auto u : tmp4){
-            s4 += A[u];
-        }
-
-        vector<ll> tmp5 = middle2;
-        tmp5.push_back(N-1);
-        ll s5 = 0;
-        for (auto u : tmp5){
-            s5 += A[u];
-        }
-
-        cout << min(min(min(min(s1, s2), s3), s4), s5) << endl;;
+    //行動１を行わない
+    //初期化
+    for (int i=1;i<=N;i++){
+        dp[i][0] = 0LL;
+        dp[i][1] = 0LL;
     }
+    dp[1][0] = 0LL;
+    dp[1][1] = LLONG_MAX;
+    for (int i=2;i<=N;i++){
+        dp[i][0] = dp[i-1][1];
+        dp[i][1] = min(dp[i-1][0], dp[i-1][1]) + A[i];
+    }
+    ll ans1 = dp[N][1];
+
+    //行動１を行う
+    //初期化
+    for (int i=1;i<=N;i++){
+        dp[i][0] = 0LL;
+        dp[i][1] = 0LL;
+    }
+    dp[1][1] = A[1];
+    dp[1][0] = LLONG_MAX;
+    for (int i=2;i<=N;i++){
+        dp[i][0] = dp[i-1][1];
+        dp[i][1] = min(dp[i-1][0], dp[i-1][1]) + A[i];
+    }
+    ll ans2 = min(dp[N][0], dp[N][1]);
+
+    cout << min(ans1, ans2) << endl;
+
     return 0;
 }
