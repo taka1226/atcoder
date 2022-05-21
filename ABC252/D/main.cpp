17176b6dc838c6507b474dacc9ac3962506047bf
taka1226 <taka2637napoleon815@icloud.com>
2022-05-21 23:12:01 +0900

    問題文の言い換え大事

--- ABC252/D/main.cpp
+++ ABC252/D/main.cpp
@@ -1,40 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll = int64_t;
-//const
-const ll MAX_N = 200005;
 
-//input
-ll N;
-ll A[MAX_N];
-map<ll, ll> cnt;
-map<ll, ll> cnt_cnt;
+//const
+const int MAX_N = 200005;
+
+//inputs
+int N;
+int A[MAX_N];
+ll cnt[MAX_N];
 
 int main(){
     cin >> N;
-    for (ll i=0;i<N;i++){
+    for (int i=0;i<N;i++){
         cin >> A[i];
         cnt[A[i]]++;
     }
 
-    for (auto [x, y] : cnt){
-        cnt_cnt[y]++;
+    for (int i=0;i<MAX_N;i++){
+        cnt[i+1] += cnt[i];
     }
 
-    ll tmp_n = N;
     ll ans = 0;
-    int i=0;
-    for (auto [s, t] : cnt){
-        if (i == N - 2) break;
-        for (auto [x, y] : cnt_cnt){
-            ans += (ll)t * (ll)x * (tmp_n - t - x) * (y - 1);
-        }
-        tmp_n -= t;
-        i++;
+    for (int i=0;i<N;i++){
+        ans += cnt[A[i] - 1] * (ll)(N - cnt[A[i]]);
     }
 
     cout << ans << endl;
-
-
     return 0;
 }
